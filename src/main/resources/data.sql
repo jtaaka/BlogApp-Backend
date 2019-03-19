3bebf3f2c524480bc0bf26035a6c23c2293ad42d
Juho Taakala <juho.taakala@cs.tamk.fi>
2019-03-19 21:41:24 +0200

    testing

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO user (id, username, password) VALUES
+  (1, 'user', 'password');
+
+INSERT INTO blogpost (id, title, content) VALUES
+  (1, 'title1', 'content1'),
+  (2, 'title2', 'content2');
