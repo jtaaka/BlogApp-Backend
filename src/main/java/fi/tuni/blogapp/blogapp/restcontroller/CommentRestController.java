74e8ade3ef843fff2716844a6ba00db79b5a201b
Juho Taakala <juho.taakala@cs.tamk.fi>
2019-04-24 22:19:31 +0300

    JavaDoc etc.

--- src/main/java/fi/tuni/blogapp/blogapp/restcontroller/CommentRestController.java
+++ src/main/java/fi/tuni/blogapp/blogapp/restcontroller/CommentRestController.java
@@ -12,12 +12,25 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Optional;
 
+/**
+ * Comment rest controller class.
+ */
 @RestController
 public class CommentRestController {
 
+    /**
+     * Defines comment repository.
+     */
     @Autowired
     CommentRepository database;
 
+    /**
+     * Fetches comment by id from database.
+     *
+     * @param id Id of comment.
+     * @return Returns comment response entity.
+     * @throws SomethingWentWrongException Throws not found by comment id.
+     */
     @RequestMapping(value = "/comment/{id}",  method= RequestMethod.GET)
     public ResponseEntity<Comment> fetchComment(@PathVariable Long id) throws SomethingWentWrongException {
         Optional<Comment> comment = database.findById(id);
@@ -27,11 +40,23 @@ public class CommentRestController {
             throw new SomethingWentWrongException("not found");
     }
 
+    /**
+     * Returns all comments from database.
+     *
+     * @return All comments from database.
+     */
     @RequestMapping(value = "/comments",  method= RequestMethod.GET)
     public Iterable<Comment> fetchAll() {
         return database.findAll();
     }
 
+    /**
+     * Deletes a comment by id from database.
+     *
+     * @param id Id of deleted
+     * @return Returns response entity with http status ok.
+     * @throws SomethingWentWrongException Throws excepion if delete fails.
+     */
     @RequestMapping(value = "/comment/{id}",  method= RequestMethod.DELETE)
     public ResponseEntity<Void> deletePost(@PathVariable Long id) throws SomethingWentWrongException {
         if (database.existsById(id)) {
@@ -42,6 +67,13 @@ public class CommentRestController {
         }
     }
 
+    /**
+     * Saves new comment to database.
+     *
+     * @param comment Comment entity to blogpost.
+     * @param b Uri components builder.
+     * @return
+     */
     @RequestMapping(value = "/comment",  method= RequestMethod.POST)
     public ResponseEntity<Comment> save(@RequestBody Comment comment, UriComponentsBuilder b) {
         database.save(new Comment(comment.getContent(), comment.getPostId()));
@@ -55,6 +87,9 @@ public class CommentRestController {
         return new ResponseEntity<>(comment, headers, HttpStatus.CREATED);
     }
 
+    /**
+     * Custom illegal argument exception class.
+     */
     @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
     private class SomethingWentWrongException extends IllegalArgumentException {
         private SomethingWentWrongException(String msg) {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO user (id, username, password) VALUES
 (1, 'admin', 'pass');
+
+INSERT INTO blogpost (id, title, content, date) VALUES
+(1, 'Otsikko', 'Sisältö', DATE '2019-04-24'),
+(2, 'Otsikko2', 'Sisältö2', DATE '2019-04-24');
